57953ae71076c10e7c5b0c40393165cdd2fd7c5f
Masayuki Ota <masota@microsoft.com>
2020-11-17 00:10:08 +0000

    add skelton AzFunc Code

---
+++ src/Models/Thermometer.cs
@@ -0,0 +1,11 @@
+namespace EhFuncIac
+{
+    public class Thermometer
+    {
+        public string SensorId { get; set; }
+        public string SensorType { get; set; }
+        public double Value { get; set; }
+        public string GeneratedDate { get; set; }
+
+    }
+}
